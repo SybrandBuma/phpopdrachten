de370550e75fedab861bf9052831a3657d302661
SybrandBuma <sybrand.baltussen@edu-kw1c.nl>
2020-02-12 12:32:25 +0100

    Opdracht 2.1 en 2 en de index

---
+++ Periode3/Hoofdstuk02/2.1/Opdracht 2.1.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: Sybrand Baltussen
+ * Date: 05/02/2020
+ * Time: 10:56 PM
+ * File: opdracht 2.1.php
+ */
+?>
+<html>
+<link href="css/stylesheet.css">
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <?php
+    echo "Hallo wereld"
+    ?>
+</main>
+<a href="../../index.php">terug</a>
+</body>
+</html>
---
+++ Periode3/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * User: Sybrand Baltussen
+ * Date: 05-02-2020
+ * Time: 11:18 AM
+ * File: index.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title><?php print "pagina 1"; ?></title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk 2
+            <ul>
+                <li>
+                    <a href="Hoofdstuk02/2.1/Opdracht 2.1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="Hoofdstuk02/2.2/opdracht 2.2.php">Opdracht 2.2</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
+</body>
+</html>
